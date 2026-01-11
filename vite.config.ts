a9d9f96821ad7803725fab02ac640661fcc4405c
Bartosz Kowal <bartoszkowal96@gmail.com>
2026-01-11 21:39:05 +0100

    configure github actions (deployment workflow)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/sorting-algorithms/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/algorithms-sorting/" : "/",
   server: {
     port: 3000,
     open: true,
